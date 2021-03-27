d81c3778d4fbb01bdb7616d558a46e58fdaa562e
Tomas <duclosiw@gmail.com>
2021-03-27 12:18:11 -0300

    adding header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+// Components
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <>
-    <h1>Hi</h1>
+    <Header isAuth={true} username="Tomas" img="https://i.pinimg.com/originals/e0/00/90/e00090ebaee8a7036a15439452006ad2.jpg"/>
     </>
   )
 }
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,9 +1,74 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {NavLink} from "react-router-dom";
+import "./scss/header.scss";
 
-const Header = () => {
+interface IHeaderProps {
+    isAuth: boolean,
+    img: string,
+    username: string
+}
+
+const Header = ({isAuth, img, username}: IHeaderProps) => {
+
+    const [displayCog, setDisplayCog] = useState(false);
+
+    useEffect(() => {
+        const hiddeOptions = () => {
+            if (displayCog) {
+                setDisplayCog(false)
+            }
+        }
+
+        window.addEventListener("click", hiddeOptions)
+
+        return () => {
+            window.removeEventListener("click", hiddeOptions)
+        }
+    }, [displayCog])
+
+    const HeaderRight = () => {
+        if (!isAuth) {
+            return (
+                <>
+                <button type="button" className="primary">Sign In</button>
+                <button type="button">Sign Up</button>
+                </>
+            )
+        }
+
+        return (
+            <>
+            <div className="iw_header__right-setting">
+                <i className="i__cog fas fa-cog" onClick={showOptions}></i>
+                <div className="iw_header__right-setting-options" style={{display: displayCog ? "flex" : "none"}}>
+                    <NavLink to="/">Profile</NavLink>
+                </div>
+            </div>
+            <img src={img} alt={username} />
+            <button type="button">Logout</button>
+            </>
+        )
+
+    }
+
+    const showOptions = () => {
+        if (!displayCog) return setDisplayCog(true)
+        return setDisplayCog(false)
+    }
+
     return (
-        <header>
+        <header className="iw_header">
+            <div className="iw_header__left">
+                <h2>JWT Login</h2>
+            </div>
 
+            <nav>
+                <ul>
+                    <li><NavLink to="/">Home</NavLink></li>
+                </ul>
+            </nav>
+
+            <div className="iw_header__right"><HeaderRight/></div>
         </header>
     )
 }
